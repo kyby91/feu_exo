d7dc57c729f5b1cf74d4cf7f49a95f06a207256f
kyby91 <julienmoullec@yahoo.fr>
2023-10-27 17:50:47 +0200

    end feu02

--- feu02.rb
+++ feu02.rb
@@ -10,10 +10,11 @@ def resoudre_expression(expression)
       parenthese_ouverte_index = expression.rindex('(')
       parenthese_fermee_index = expression.index(')', parenthese_ouverte_index)
       sous_expression = expression[(parenthese_ouverte_index + 1)...parenthese_fermee_index]
-      p sous_expression
+
       resultat_sous_expression = resoudre_expression(sous_expression)
   
-      expression.sub!(sous_expression, resultat_sous_expression.to_s)
+      expression.sub!("#{expression[parenthese_ouverte_index..parenthese_fermee_index]}", resultat_sous_expression.to_s)
+      
     end
   
     # Étape 3 : Résolvez les opérations de multiplication et de division de gauche à droite
@@ -56,48 +57,10 @@ def resoudre_expression(expression)
     end
   
     # Étape 5 : Le résultat final est le seul élément restant dans l'expression
-    resultat_final = expression.to_i
-    puts "Le résultat est : #{resultat_final}"
+    return expression.to_i
+    
 end
 
 # Appel de la fonction avec l'expression à résoudre
-resoudre_expression("4 + 21 * 38 + (1+1)")  # Exemple avec priorité de multiplication
-  
-
-def resoudre_operation(number1, operator, number2)
-    # Étape 1 : Séparez les différents éléments de l'opération
-
-    # elements = operation.split(" ") #!!!!ATTENTION!!!!!
-  
-    # Étape 2 : Vérifiez l'opérateur et effectuez l'opération appropriée
-    resultat = case operator
-               when "+"
-                 number1.to_i + number2.to_i
-               when "-"
-                 number1.to_i - number2.to_i
-               when "*"
-                 number1.to_i * number2.to_i
-               when "/"
-                 number1.to_i / number2.to_i
-               when "%"
-                 number1.to_i % number2.to_i
-               else
-                 puts "Opérateur invalide"
-                 return
-               end
-  
-    # Étape 3 : Affichez le résultat
-    puts "Le résultat est : #{resultat}"
-end
-
-# resoudre_operation(ARGV[0], ARGV[1], ARGV[2])
-# resoudre_operation("2","*","10")
-
-def test (expression)
-  pattern = /(\d+\*\d+)/
-  match = expression.match(pattern)
-  result = match[1] if match
-  puts result
-end
-
-# test("2+4*60+5")
+resultat_final = resoudre_expression(ARGV[0])  # Exemple avec priorité de multiplication
+puts "Le résultat est : #{resultat_final}"
